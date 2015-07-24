f28733562f2429acfb31f3a64d50887268bc786f
Sharif Olorin <sio@tesser.org>
2015-07-24 16:55:07 +1000

    IO prop test for xSV tokenizer

---
+++ test/Test/IO/Warden/IO.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell   #-}
+
+module Test.IO.Warden.IO where
+
+import P
+
+import Control.Monad.Trans.Either
+import qualified Data.ByteString.Lazy as BL
+import Data.Csv
+import qualified Data.Text as T
+import qualified Data.Vector as V
+import System.IO
+import System.IO.Temp
+import System.FilePath
+import qualified Pipes.Prelude as PP
+import Test.QuickCheck
+import Test.QuickCheck.Instances ()
+
+import Disorder.Core.IO
+
+import Test.Warden.Arbitrary
+
+import Warden.Data
+import Warden.Error
+import Warden.IO
+
+prop_svrows :: SVSep -> FieldCount -> RowCount -> Property
+prop_svrows s i n = forAll (vectorOf (getRowCount n) $ validSVRow s i) $ \svrs ->
+  testIO $ withSystemTempDirectory "warden-test" $ \tmp -> do
+    let fp = tmp </> "valid_sv"
+    BL.writeFile fp $ encodeWith opts svrs
+    BL.writeFile "/tmp/xsv" $ encodeWith opts svrs
+    res <- withFile fp ReadMode $ \h -> do
+      runEitherT $ PP.fold (flip (:)) [] id $ readSVRows (getSVSep s) h
+    case res of
+      Left err -> fail . T.unpack $ renderWardenError err
+      Right (SVEOF:rs) -> do
+        let expected = reverse $ (SVFields . V.fromList . getValidSVRow) <$> svrs
+        pure $ expected === rs
+      Right x -> fail $ "impossible result from decoding: " <> show x
+ where
+  opts = defaultEncodeOptions { encDelimiter = getSVSep s }
+
+return []
+tests :: IO Bool
+tests = $forAllProperties $ verboseCheckWithResult (stdArgs { maxSuccess = 10 })
---
+++ test/Test/Warden/Arbitrary.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Test.Warden.Arbitrary where
+
+import P
+
+import qualified Data.ByteString as BS
+import Data.Csv
+import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8)
+import Data.Word
+import Test.QuickCheck
+
+newtype SVSep = SVSep { getSVSep :: Word8 }
+  deriving (Eq, Show, Ord)
+
+instance Arbitrary SVSep where
+  arbitrary = elements $ SVSep <$> ([32..64] <> [91..96] <> [123..126])
+
+newtype ValidSVRow = ValidSVRow { getValidSVRow :: [Text] }
+  deriving (Eq, Show, Ord, ToRecord)
+
+newtype RowCount = RowCount { getRowCount :: Int }
+  deriving (Eq, Show, Ord)
+
+instance Arbitrary RowCount where
+  arbitrary = RowCount <$> choose (1, 100)
+
+newtype FieldCount = FieldCount { getFieldCount :: Int }
+  deriving (Eq, Show, Ord)
+
+instance Arbitrary FieldCount where
+  arbitrary = FieldCount <$> choose (1, 10)
+
+instance Arbitrary Text where
+
+validSVField :: SVSep
+             -> Gen Text
+validSVField (SVSep s) = (decodeUtf8 . BS.pack) <$>
+  (listOf1 arbitrary) `suchThat` (all validWord)
+ where
+  validWord w = w /= s && w <= 127
+
+validSVRow :: SVSep -> FieldCount -> Gen ValidSVRow
+validSVRow s (FieldCount n) = ValidSVRow <$> vectorOf n (validSVField s)
