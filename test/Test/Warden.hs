3d7ea3e7cf3fd0a60edfcad75470e334ed2b8099
Sharif Olorin <sio@tesser.org>
2015-07-29 05:23:33 +0000

    Prop tests for numeric folds

--- test/Test/Warden.hs
+++ test/Test/Warden.hs
@@ -5,10 +5,12 @@
 module Test.Warden where
 
 import           Control.Lens              hiding (each)
-import qualified Data.Aeson                as A
+import           Data.AEq                  (AEq)
+import qualified Data.AEq                  as AEQ
 import           Data.Attoparsec.Text
 import qualified Data.Map                  as M
 import qualified Data.Vector               as V
+import           Disorder.Aeson
 import           Disorder.Core.Tripping
 import           P
 import           Pipes
@@ -36,7 +38,47 @@ prop_roundtrip_parsed_field :: ParsedField -> Property
 prop_roundtrip_parsed_field = tripping renderParsedField (parseOnly field)
 
 prop_roundtrip_json_numericsummary :: NumericSummary -> Property
-prop_roundtrip_json_numericsummary = tripping A.encode A.decode
+prop_roundtrip_json_numericsummary = jsonProp
+
+prop_updateminimum_positive :: Minimum -> Property
+prop_updateminimum_positive mn@(Minimum Nothing) = forAll (arbitrary :: Gen Double) $ \x ->
+  (updateMinimum mn x) === (Minimum (Just x))
+prop_updateminimum_positive mn@(Minimum (Just c)) = forAll ((arbitrary :: Gen Double) `suchThat` (< c)) $ \x ->
+  (updateMinimum mn x) === (Minimum (Just x))
+
+prop_updatemaximum_positive :: Maximum -> Property
+prop_updatemaximum_positive mn@(Maximum Nothing) = forAll (arbitrary :: Gen Double) $ \x ->
+  (updateMaximum mn x) === (Maximum (Just x))
+prop_updatemaximum_positive mx@(Maximum (Just c)) = forAll ((arbitrary :: Gen Double) `suchThat` (> c)) $ \x ->
+  (updateMaximum mx x) === (Maximum (Just x))
+
+prop_updateminimum_negative :: Property
+prop_updateminimum_negative =
+  forAll (arbitrary :: Gen Double) $ \c ->
+    forAll ((arbitrary :: Gen Double) `suchThat` (>= c)) $ \x ->
+      let mn = Minimum (Just c)
+      in (updateMinimum mn x) === mn
+
+prop_updatemaximum_negative :: Property
+prop_updatemaximum_negative =
+  forAll (arbitrary :: Gen Double) $ \c ->
+    forAll ((arbitrary :: Gen Double) `suchThat` (<= c)) $ \x ->
+      let mx = Maximum (Just c)
+      in (updateMaximum mx x) === mx
+
+(~~~) :: (AEq a, Show a) => a -> a -> Property
+x ~~~ y = counterexample cex prop
+  where
+    cex          = concat ["|", show x, " - ", show y, "| > ɛ"]
+    prop         = x AEQ.~== y
+
+prop_updatemean :: Int -> Property
+prop_updatemean n = forAll (vectorOf n (arbitrary :: Gen Double)) $ \xs ->
+  let soq = getMean $ foldl (updateMean n) (Mean Nothing) xs
+      qos = if n > 0
+              then Just ((sum xs) / (fromIntegral n))
+              else Nothing
+  in soq ~~~ qos
 
 return []
 tests :: IO Bool
