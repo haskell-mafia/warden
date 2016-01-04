2a63d29752dc3745f83216819ce78f65111f2618
Sharif Olorin <sio@tesser.org>
2016-01-04 01:50:58 +0000

    Test _-prefixed files in view traversal

--- test/test-io.hs
+++ test/test-io.hs
@@ -1,12 +1,12 @@
 import           Disorder.Core.Main
 
-import qualified Test.IO.Warden.Check
+import qualified Test.IO.Warden.Check.File
 import qualified Test.IO.Warden.Rows
 import qualified Test.IO.Warden.View
 
 main :: IO ()
 main = disorderMain [
            Test.IO.Warden.Rows.tests
-         , Test.IO.Warden.Check.tests
+         , Test.IO.Warden.Check.File.tests
          , Test.IO.Warden.View.tests
          ]
