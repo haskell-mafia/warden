8859574505440ecfcfff13532ed258853b81020a
Sharif Olorin <sio@tesser.org>
2016-01-08 00:13:25 +0000

    Add CLI test on small generated view

--- test/test-cli.hs
+++ test/test-cli.hs
@@ -2,4 +2,7 @@ import           Disorder.Core.Main
 
 main :: IO ()
 main =
-  disorderCliMain ["./dist/build/warden/warden"]
+  disorderCliMain [
+      "./dist/build/warden/warden"
+    , "./dist/build/warden-gen/warden-gen"
+    ]
