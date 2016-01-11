24c1814a3f756c5ec600087f90752f223a85e5e6
Sharif Olorin <sio@tesser.org>
2016-01-11 23:02:04 +0000

    Rejig view generator for reuse in benchmarks

--- test/Test/IO/Warden.hs
+++ test/Test/IO/Warden.hs
@@ -2,9 +2,16 @@
 
 module Test.IO.Warden where
 
+import           Control.Concurrent.Async (mapConcurrently)
+
+import qualified Data.ByteString.Lazy as BL
+import           Data.Char (ord)
+import           Data.Csv (EncodeOptions(..), defaultEncodeOptions, encodeWith)
+import           Data.List (nub)
 import qualified Data.Text as T
 
 import           Disorder.Core.IO (testIO)
+import           Disorder.Corpus (muppets)
 
 import           P
 
@@ -15,6 +22,7 @@ import           System.IO.Temp (withTempFile, withTempDirectory)
 import           System.Posix.Directory (getWorkingDirectory)
 
 import           Test.QuickCheck (Gen, Testable, Property, forAll, arbitrary)
+import           Test.QuickCheck (generate, suchThat, elements, resize)
 import           Test.Warden.Arbitrary
 
 import           Warden.Data
@@ -59,3 +67,45 @@ withDirTree dt a = testIO . withTempDirectory "." "test-view" $ \tmp -> do
   let v'              = View $ tmp </> unDirName v
   writeView tmp dt
   a v'
+
+newtype LineSize =
+  LineSize {
+    unLineSize :: Int
+  } deriving (Eq, Show)
+
+newtype RecordCount =
+  RecordCount Int
+  deriving (Eq, Show)
+
+newtype GenSize =
+  GenSize Int
+  deriving (Eq, Show)
+
+generateView :: RecordCount -> GenSize -> LineSize -> IO FilePath
+generateView (RecordCount n) (GenSize s) ll = do
+  dt <- generate . resize s . fmap unValidDirTree $
+          arbitrary `suchThat` ((> 0) . length . directoryFiles . unValidDirTree)
+  tok <- generate $ elements muppets
+  fieldCount <- generate . resize (unLineSize ll) $ arbitrary
+  let viewRoot = "./warden-gen-" <> T.unpack tok
+  let dfs = nub . fmap (viewRoot </>) $ directoryFiles dt
+  let fileLines = n `div` (length dfs)
+  writeView viewRoot dt
+  void $ mapConcurrently (writeViewFile fileLines fieldCount ll) dfs
+  pure $ viewRoot </> (viewBase dt)
+  where
+    viewBase (DirTree b _ _) = unDirName b
+
+writeViewFile :: Int -> FieldCount -> LineSize -> FilePath -> IO ()
+writeViewFile c fc (LineSize ll) fp = do
+  fh <- openFile fp WriteMode
+  replicateM_ c (writeRow fh)
+  hClose fh
+  where
+    sep = Separator . fromIntegral $ ord '|'
+
+    writeRow h = do
+      r <- generate . resize ll $ validSVRow sep fc
+      BL.hPutStr h $ encodeWith opts [r]
+
+    opts = defaultEncodeOptions { encDelimiter = unSeparator sep }
