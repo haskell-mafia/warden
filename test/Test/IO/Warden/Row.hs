4251adb0e50797975fc412d1ce1e8c54d8beee27
Sharif Olorin <sio@tesser.org>
2016-01-12 04:42:38 +0000

    Add IO tests for conduit implementation

--- test/Test/IO/Warden/Row.hs
+++ test/Test/IO/Warden/Row.hs
@@ -4,8 +4,11 @@
 
 module Test.IO.Warden.Row where
 
+import           Control.Monad.Trans.Resource (runResourceT)
 
 import qualified Data.ByteString.Lazy       as BL
+import           Data.Conduit (($$))
+import qualified Data.Conduit.List as CL
 import           Data.Csv
 import qualified Data.Text                  as T
 import qualified Data.Vector                as V
@@ -30,8 +33,8 @@ import           Warden.Row
 
 import           X.Control.Monad.Trans.Either
 
-prop_valid_svrows :: Separator -> FieldCount -> RowCount -> Property
-prop_valid_svrows s i n = forAll (vectorOf (unRowCount n) $ validSVRow s i) $ \svrs ->
+prop_valid_svrows_pipes :: Separator -> FieldCount -> RowCount -> Property
+prop_valid_svrows_pipes s i n = forAll (vectorOf (unRowCount n) $ validSVRow s i) $ \svrs ->
   testIO $ withSystemTempDirectory "warden-test" $ \tmp -> do
     let fp = tmp </> "valid_sv"
     BL.writeFile fp $ encodeWith opts svrs
@@ -45,13 +48,41 @@ prop_valid_svrows s i n = forAll (vectorOf (unRowCount n) $ validSVRow s i) $ \s
  where
    opts = defaultEncodeOptions { encDelimiter = unSeparator s }
 
-prop_invalid_svrows :: Separator -> RowCount -> Property
-prop_invalid_svrows s n = forAll (vectorOf (unRowCount n) (invalidSVRow s)) $ \svrs ->
+prop_invalid_svrows_pipes :: Separator -> RowCount -> Property
+prop_invalid_svrows_pipes s n = forAll (vectorOf (unRowCount n) (invalidSVRow s)) $ \svrs ->
   testIO $ withSystemTempDirectory "warden-test" $ \tmp -> do
     let fp = tmp </> "sv"
     BL.writeFile fp $ (BL.intercalate "\r\n") svrs
     res <- withFile fp ReadMode $ \h -> do
       runEitherT $ PP.fold (flip (:)) [] id $ readSVHandle s h
+    case res of
+      Left err -> fail . T.unpack $ renderWardenError err
+      Right rs ->
+        pure $ [] === filter (not . rowFailed) rs
+ where
+  rowFailed (RowFailure _) = True
+  rowFailed _              = False
+
+prop_valid_svrows_conduit :: Separator -> FieldCount -> RowCount -> Property
+prop_valid_svrows_conduit s i n = forAll (vectorOf (unRowCount n) $ validSVRow s i) $ \svrs ->
+  testIO $ withSystemTempDirectory "warden-test" $ \tmp -> do
+    let fp = tmp </> "valid_sv"
+    BL.writeFile fp $ encodeWith opts svrs
+    res <- runEitherT . mapEitherT runResourceT $ readViewFile s (LineBound 65536) (ViewFile fp) $$ CL.fold (flip (:)) []
+    case res of
+      Left err -> fail . T.unpack $ renderWardenError err
+      Right rs -> do
+        let expected = reverse $ (SVFields . V.fromList . getValidSVRow) <$> svrs
+        pure $ expected === rs
+ where
+   opts = defaultEncodeOptions { encDelimiter = unSeparator s }
+
+prop_invalid_svrows_conduit :: Separator -> RowCount -> Property
+prop_invalid_svrows_conduit s n = forAll (vectorOf (unRowCount n) (invalidSVRow s)) $ \svrs ->
+  testIO $ withSystemTempDirectory "warden-test" $ \tmp -> do
+    let fp = tmp </> "sv"
+    BL.writeFile fp $ (BL.intercalate "\r\n") svrs
+    res <- runEitherT . mapEitherT runResourceT $ readViewFile s (LineBound 65536) (ViewFile fp) $$ CL.fold (flip (:)) []
     case res of
       Left err -> fail . T.unpack $ renderWardenError err
       Right rs ->
