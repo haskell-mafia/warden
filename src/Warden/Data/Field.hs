66c2a026f1ed3c27d088f82cead0f0166b202d30
Sharif Olorin <sio@tesser.org>
2016-03-08 04:23:25 +0000

    Add inclusion relation for field-type lattice

--- src/Warden/Data/Field.hs
+++ src/Warden/Data/Field.hs
@@ -3,9 +3,12 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DeriveGeneric #-}
 
+{- This module will probably live in brandix soon. -}
+
 module Warden.Data.Field (
     FieldLooks(..)
   , FieldType(..)
+  , fieldTypeIncludes
   ) where
 
 import           Data.Ix (Ix)
@@ -14,14 +17,13 @@ import           GHC.Generics (Generic)
 
 import           P
 
--- FIXME: unknown type
 data FieldType =
     TextField
   | CategoricalField
   | BooleanField
   | IntegralField
   | RealField
-  deriving (Eq, Show, Enum, Bounded, Ix, Generic)
+  deriving (Eq, Show, Enum, Bounded, Ord, Ix, Generic)
 
 instance NFData FieldType
 
@@ -37,3 +39,27 @@ data FieldLooks =
 
 instance NFData FieldLooks
 
+-- | Schema field types kinda look like a join semilattice under set
+-- inclusion. For example, if a given field is classified as a 
+-- 'CategoricalField', it's not an anomaly (from the fairly limited
+-- perspective of schema inference) if some of its values are classified as
+-- 'LooksBoolean' (e.g., from the set {"true", "false",
+-- "undetermined"}). Likewise e.g., 'LooksIntegral' $$ \subseteq $$
+-- 'RealField'.
+--
+-- FIXME: come up with something easier to test than this horrible pattern-match
+fieldTypeIncludes :: FieldType -> FieldLooks -> Bool
+fieldTypeIncludes _ LooksEmpty = True
+fieldTypeIncludes TextField _ = True
+
+-- equality
+fieldTypeIncludes CategoricalField LooksCategorical = True
+fieldTypeIncludes BooleanField LooksBoolean = True
+fieldTypeIncludes IntegralField LooksIntegral = True
+fieldTypeIncludes RealField LooksReal = True
+
+-- set inclusion
+fieldTypeIncludes RealField LooksIntegral = True
+fieldTypeIncludes CategoricalField LooksBoolean = True
+
+fieldTypeIncludes _ _ = False
