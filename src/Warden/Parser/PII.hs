4582591b87aee98a76917673dade98d13013fed7
Sharif Olorin <sio@tesser.org>
2016-04-18 05:11:05 +0000

    Add phone number parsers

--- src/Warden/Parser/PII.hs
+++ src/Warden/Parser/PII.hs
@@ -6,13 +6,15 @@
 
 module Warden.Parser.PII (
     emailP
+  , phoneNumberP
   ) where
 
 import           Data.Attoparsec.ByteString (Parser)
 import           Data.Attoparsec.ByteString (takeWhile1, word8, endOfInput)
+import           Data.Attoparsec.ByteString (skipMany, satisfy, choice, count)
 import           Data.Char (ord)
 
-import           P
+import           P hiding (count)
 
 import           Warden.Parser.Common
 
@@ -32,4 +34,52 @@ emailP = do
     hostPart = not . flip elem [space, period, at]
 #ifndef NOINLINE
 {-# INLINE emailP #-}
+#endif
+
+-- | Matches Australian phone numbers or fully-qualified international phone
+-- numbers.
+phoneNumberP :: Parser ()
+phoneNumberP =
+  void $ choice [australianNumberP, internationalNumberP]
+#ifndef NOINLINE
+{-# INLINE phoneNumberP #-}
+#endif
+
+australianNumberP :: Parser ()
+australianNumberP = do
+  void $ word8 zero
+  void $ count 9 phoneCharP
+  where
+    zero = fromIntegral $ ord '0'
+#ifndef NOINLINE
+{-# INLINE australianNumberP #-}
+#endif
+
+internationalNumberP :: Parser ()
+internationalNumberP = do
+  void $ word8 plus
+  void $ count 11 phoneCharP
+  where
+    plus = fromIntegral $ ord '+'
+#ifndef NOINLINE
+{-# INLINE internationalNumberP #-}
+#endif
+
+phoneCharP :: Parser ()
+phoneCharP = satisfy isPhoneChar >> skipPhoneFiller
+  where
+    isPhoneChar c = c >= 0x30 && c <= 0x39 -- 0-9
+#ifndef NOINLINE
+{-# INLINE phoneCharP #-}
+#endif
+
+skipPhoneFiller :: Parser ()
+skipPhoneFiller = skipMany (satisfy valid)
+  where
+    valid 0x20 = True -- space
+    valid 0x2d = True -- hyphen
+    valid 0x2e = True -- period
+    valid _ = False
+#ifndef NOINLINE
+{-# INLINE skipPhoneFiller #-}
 #endif
--- test/Test/Warden/Parser/PII.hs
+++ test/Test/Warden/Parser/PII.hs
@@ -5,6 +5,7 @@
 module Test.Warden.Parser.PII where
 
 import           Data.Attoparsec.ByteString (parseOnly)
+import qualified Data.Text.Encoding as T
 
 import           Disorder.Corpus (muppets)
 
@@ -25,6 +26,17 @@ prop_emailP_pos = forAll genEmail $ \e ->
 prop_emailP_neg :: Property
 prop_emailP_neg = forAll (elements muppets) $ \e ->
   let r = parseOnly emailP e in
+  isLeft r === True
+
+prop_phoneNumberP_pos :: Property
+prop_phoneNumberP_pos = forAll genPhoneNumber $ \n ->
+  let r = parseOnly phoneNumberP n in
+  isRight r === True
+
+prop_phoneNumberP_neg :: Int -> Property
+prop_phoneNumberP_neg n =
+  let n' = T.encodeUtf8 $ renderIntegral n
+      r = parseOnly phoneNumberP n' in
   isLeft r === True
 
 return []
