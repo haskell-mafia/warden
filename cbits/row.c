fcf137cab182e39254be09a36db22773120d7e45
Sharif Olorin <sio@tesser.org>
2016-05-04 04:25:14 +0000

    Faster implementation of asciiToLower because gcc is dumb

--- cbits/row.c
+++ cbits/row.c
@@ -5,9 +5,16 @@
 #include "row.h"
 
 void warden_ascii_to_lower(size_t n, int8_t *out, int8_t *in) {
-	int i;
+	int i = 0;
 	memcpy(out, in, n);
-	for (i = 0; i < n; i++) {
+	if (n >= 8) {
+		for ( ; i < n; i += 8) {
+			int64_t *p = (int64_t *) &out[i];
+			*p |= 0x2020202020202020;
+		}
+		i -= 8;
+	}
+	for ( ; i < n; i++) {
 		out[i] |= 0x20;
 	}
 }
