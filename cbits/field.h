a9fbd3562a3d4e4f5c05774eecdd7c4f6fe44def
Sharif Olorin <sio@tesser.org>
2016-05-17 07:00:58 +0000

    C parser for numeric fields

--- cbits/field.h
+++ cbits/field.h
@@ -6,6 +6,14 @@
 
 #include "warden.h"
 
+typedef enum _numeric_field {
+	non_numeric_field = 0,
+	integral_field = 1,
+	real_field = 2
+} numeric_field;
+
 bool warden_field_bool(char *, size_t);
+
+numeric_field warden_field_numeric(char *, size_t);
 
 #endif
--- test/Test/Warden/Parser/Field.hs
+++ test/Test/Warden/Parser/Field.hs
@@ -29,6 +29,33 @@ prop_checkFieldBool_neg = forAll (T.encodeUtf8 <$> renderedNonBool) $ \bs ->
   let r = checkFieldBool bs in
   r === False
 
+prop_checkFieldNumeric_pos :: Int -> Double -> Property
+prop_checkFieldNumeric_pos n m =
+  let bs1 = T.encodeUtf8 $ renderIntegral n
+      bs2 = T.encodeUtf8 $ renderFractional m
+      r1 = checkFieldNumeric bs1
+      r2 = checkFieldNumeric bs2 in
+  (r1, r2) === (Just' LooksIntegral, Just' LooksReal)
+
+prop_checkFieldNumeric_neg :: Int -> Double -> Property
+prop_checkFieldNumeric_neg n m = forAll (elements muppets) $ \b ->
+  let p1 = T.encodeUtf8 $ renderIntegral n
+      p2 = T.encodeUtf8 $ renderFractional m
+      b' = T.encodeUtf8 b
+      bss = [
+          b'
+        , b' <> p1
+        , b' <> p2
+        , p1 <> b'
+        , p2 <> b'
+        , b' <> p1 <> b'
+        , b' <> p2 <> b'
+        , p1 <> b' <> p1
+        , p2 <> b' <> p2
+        ]
+      r = all (== Nothing') $ checkFieldNumeric <$> bss in
+  r === True
+
 prop_numericFieldP_pos :: Int -> Double -> Property
 prop_numericFieldP_pos n m =
   let tn = T.encodeUtf8 $ renderIntegral n
