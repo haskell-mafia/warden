5424f22bd27f1007f2d79f80bacd23b1ab37b497
Sharif Olorin <sio@tesser.org>
2016-07-19 07:20:43 +0000

    Fail if someone tries to compile us on a big-endian CPU
    Unlikely as that may be. Have a bunch of LE-specific optimisations in place.

--- cbits/warden.h
+++ cbits/warden.h
@@ -4,6 +4,12 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#include <endian.h>
+
+#if __BYTE_ORDER != __LITTLE_ENDIAN
+#error "warden only works on little-endian architectures."
+#endif
+
 typedef uint8_t bool;
 
 #define TRUE 1
