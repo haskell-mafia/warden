67d49e6878ce8d32441335d600f1c6649d73f368
Sharif Olorin <sio@tesser.org>
2017-04-20 01:46:58 +0000

    wibble

---
+++ test/Test/IO/Warden/Commands/Sample.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+
+module Test.IO.Warden.Commands.Sample where
+
+import           Control.Monad.IO.Class (liftIO)
+
+import           Disorder.Core.IO (testIO)
+
+import           P
+
+import           System.Directory (removeFile)
+import           System.IO
+
+import           Test.QuickCheck
+import           Test.QuickCheck.Instances ()
+
+import           Test.IO.Warden
+import           Test.Warden.Arbitrary ()
+
+import           Warden.Data
+import           Warden.Marker
+
+import           X.Control.Monad.Trans.Either (bracketEitherT')
+
+prop_writeViewMarker :: NonEmpty ViewMarker -> Property
+prop_writeViewMarker vms =
+  forAll genNumericFieldSummary $ \nfs ->
+  testIO $ withTestView $ \v -> unsafeWarden $ do
+  let
+    vms' = fmap (\vm -> vm { vmView = v }) vms
+    vms'' = fmap (setNumericFieldSummary nfs) vms'
+
+  writeViewMarker vm'
+  vm'' <- readViewMarker $ viewMarkerPath vm'
+  pure $ vm'' === vm'
+
+setNumericFieldSummary :: NumericFieldSummary -> ViewMarker -> ViewMarker
+setNumericFieldSummary nfs vm =
+  let
+    rcs = vmViewCounts $ vmMetadata vm
+    rcs' = rcs { rcsNumericSummary = nfs }
+    meta' = vmMetadata vm { vmViewCounts = rcs' }
+  in
+  vm { vmMetadata = meta' }
+
+return []
+tests :: IO Bool
+tests = $forAllProperties $ quickCheckWithResult (stdArgs { maxSuccess = 10 })
